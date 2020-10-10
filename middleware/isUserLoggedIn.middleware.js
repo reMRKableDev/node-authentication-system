6b9e4738fe26164a325658fffad2fdb5ca1d0420
Malcolm Kente <malcolm.kente@remrkabledev.com>
2020-10-10 22:06:16 +0200

    :bulb: Added jsdocs for middleware

--- middleware/isUserLoggedIn.middleware.js
+++ middleware/isUserLoggedIn.middleware.js
@@ -1,3 +1,10 @@
+/**
+ * Check if the user is logged in
+ * @module
+ * @param {*} req
+ * @param {*} res
+ * @param {*} next
+ */
 module.exports = (req, res, next) => {
   if (req.session.user && req.cookies.nodeCookie) {
     res.redirect("/profile");
