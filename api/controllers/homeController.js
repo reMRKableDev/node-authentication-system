5416dad452d6ff74b8f0f13c4e259dd93ca26467
Malcolm Kente <malcolm.kente@remrkabledev.com>
2020-10-12 23:29:47 +0200

    :pencil: Added jsdocs to home controller

--- api/controllers/homeController.js
+++ api/controllers/homeController.js
@@ -1,3 +1,10 @@
-module.exports = (req, res) => {
-  res.render("index");
-};
+/**
+ * Home route controller
+ * @module
+ * @function
+ *  
+ * @param {object} _ - Unused request argument
+ * @param {object} res - Outbound response object 
+ * @returns {string} - Value of a view to be rendered
+ */
+module.exports = (_, res) => res.status(200).render("index");
